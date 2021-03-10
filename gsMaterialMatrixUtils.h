13fa4cee7b0b8c0d088a5b27f864f4454733f3b9
Hugo Verhelst <hugoverhelst1995@gmail.com>
2021-03-10 09:14:19 +0100

    Documentation for gsMaterialMatrixUtils

--- gsMaterialMatrixUtils.h
+++ gsMaterialMatrixUtils.h
@@ -21,6 +21,16 @@
 namespace gismo
 {
 
+
+/**
+ * @brief      This class describes if an object is integrated through-thickness or not.
+ *
+ * NotIntegrated: The object has to be integrated
+ * Integrated: The object is integrated
+ * Constant: The object is constant through thickness, but is not integrated
+ * Linear: The object is linear through thickness, but is not integrated
+ *
+ */
 enum class MatIntegration : short_t
 {
     NotIntegrated   = 0,
@@ -29,6 +39,10 @@ enum class MatIntegration : short_t
     Linear          = 3,
 };
 
+
+/**
+ * @brief      This class describes a material model.
+ */
 enum class Material : short_t
 {
     SvK = 0,
@@ -38,6 +52,15 @@ enum class Material : short_t
     OG  = 4
 };
 
+
+/**
+ * @brief      This class describes the way material models are implemented.
+ *
+ * Composite: laminate material model
+ * Analytical: The expressions for Cijkl and Sij have to be provided in closed form
+ * Generalized: Uses a generalized way, where only the derivatives of psi have to be implemented
+ * Spectral: Implementation based on derivatives of psi w.r.t. principal stretches
+ */
 enum class Implementation : short_t
 {
     Composite   = 0,
@@ -46,6 +69,23 @@ enum class Implementation : short_t
     Spectral    = 3
 };
 
+
+/**
+ * @brief      This class describes the output type.
+ *
+ * Generic: unspecified output type
+ * Density
+ * VectorN: Membrane forces
+ * VectorM: Bending moments
+ * MatrixA: 0th order moment of the differential of the material matrix w.r.t. membrane strains
+ * MatrixB: 1st order moment of the differential of the material matrix w.r.t. membrane strains
+ * MatrixC: 0th order moment of the differential of the material matrix w.r.t. bending strains
+ * MatrixD: 1st order moment of the differential of the material matrix w.r.t. bending strains
+ * PStressN: membrane principal stress
+ * PStressN: bending principal stress
+ * Stretch: Principal stretch
+ * StretchDir: Principal stretch directions
+ */
 enum class MaterialOutput : short_t
 {
     Generic = 0,
@@ -62,11 +102,23 @@ enum class MaterialOutput : short_t
     StretchDir = 11,
 };
 
+
+/**
+ * @brief      Encodes the material model and implementation
+ *
+ * @tparam     material        Material model
+ * @tparam     implementation  The way it is implemented
+ */
 template<enum Material material, enum Implementation implementation>
 struct encodeMat_id {
   static const constexpr short_t id = 10*(short_t)implementation + (short_t)material;
 };
 
+/**
+ * @brief      Decodes the material model and implementation
+ *
+ * @tparam     id    identifier (from encoder)
+ */
 template<short_t id>
 struct decodeMat_id {
   static const constexpr enum Material          material = (enum Material)(id%10);
