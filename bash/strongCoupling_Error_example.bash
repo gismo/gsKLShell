d79367d9ae22c623538573d5477b1bf771b4bf4a
Hugo Verhelst <hugoverhelst1995@gmail.com>
2022-04-24 00:42:05 +0200

    small in bash

--- bash/strongCoupling_Error_example.bash
+++ bash/strongCoupling_Error_example.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Declare an array of string with type
 declare -a Runname="../../../build/bin/strongCoupling_Error_example"
-declare -a Filenames=("1p_hyperboloid" "3p_hyperboloid" "4p_hyperboloid" "6p_hyperboloid" )
+declare -a Filenames=("1p_hyperboloid" "3p_hyperboloid" "4p_hyperboloid" "6p_hyperboloid" "4p_hyperboloid_hole_deg4deg4")
 declare -a Methods=(1 2 3 4)
 
 mkdir -p Output
@@ -9,9 +9,9 @@ mkdir -p Output
 for file in ${Filenames[@]}; do
     for (( p=2; p<4; p++)) do  
         for m in ${Methods[@]}; do
-            echo "$Runname" -m $m -p $p -s $(($p-1)) -G ../extensions/gsKLShell/filedata/pde/"$file"_geom.xml -B ../extensions/gsKLShell/filedata/pde/"$file"_bvp.xml -C 1e3 -r 3
+            echo "$Runname" -m $m -p $p -s $(($p-1)) -G ../extensions/gsKLShell/filedata/pde/"$file"_geom.xml -B ../extensions/gsKLShell/filedata/pde/"$file"_bvp.xml -C 1e3 -r 5
             
-            "$Runname" -m $m -p $p -s $(($p-1)) -G ../extensions/gsKLShell/filedata/pde/"$file"_geom.xml -B ../extensions/gsKLShell/filedata/pde/"$file"_bvp.xml -C 1e3 -r 3 1> Output/"$file"_p"$p"_m"$m".log 2> Output/"$file"_p"$p"_m"$m".log
+            "$Runname" -m $m -p $p -s $(($p-1)) -G ../extensions/gsKLShell/filedata/pde/"$file"_geom.xml -B ../extensions/gsKLShell/filedata/pde/"$file"_bvp.xml -C 1e3 -r 5 1> Output/"$file"_p"$p"_m"$m".log 2> Output/"$file"_p"$p"_m"$m".log
         done
     done
 done
--- bash/weakCoupling_Error_example.bash
+++ bash/weakCoupling_Error_example.bash
@@ -1,14 +1,14 @@
 #!/bin/bash
 # Declare an array of string with type
 declare -a Runname="../../../build/bin/weakCoupling_Error_example"
-declare -a Filenames=("1p_hyperboloid" "3p_hyperboloid" "4p_hyperboloid" "6p_hyperboloid" )
+declare -a Filenames=("1p_hyperboloid" "3p_hyperboloid" "4p_hyperboloid" "6p_hyperboloid" "4p_hyperboloid_hole_deg4deg4")
 
 mkdir -p Output
   
 for file in ${Filenames[@]}; do
     for (( p=2; p<4; p++)) do  
-        echo "$Runname" -p $p -s $(($p-1)) -G ../extensions/gsKLShell/filedata/pde/"$file"_geom.xml -B ../extensions/gsKLShell/filedata/pde/"$file"_bvp.xml -C 1e3 -r 3
+        echo "$Runname" -p $p -s $(($p-1)) -G ../extensions/gsKLShell/filedata/pde/"$file"_geom.xml -B ../extensions/gsKLShell/filedata/pde/"$file"_bvp.xml -C 1e3 -r 5
             
-        "$Runname" -p $p -s $(($p-1)) -G ../extensions/gsKLShell/filedata/pde/"$file"_geom.xml -B ../extensions/gsKLShell/filedata/pde/"$file"_bvp.xml -C 1e3 -r 3 1> Output/"$file"_p"$p"_weak.log 2> Output/"$file"_p"$p"_weak.log
+        "$Runname" -p $p -s $(($p-1)) -G ../extensions/gsKLShell/filedata/pde/"$file"_geom.xml -B ../extensions/gsKLShell/filedata/pde/"$file"_bvp.xml -C 1e3 -r 5 1> Output/"$file"_p"$p"_weak.log 2> Output/"$file"_p"$p"_weak.log
     done
 done
