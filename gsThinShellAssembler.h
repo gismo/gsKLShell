b530a80d673e9d24574241b0d4e5ef1abf300d2b
Crazy-Rich-Meghan <delft2021li@gmail.com>
2023-08-14 15:50:28 +0200

    missing bracket

--- gsThinShellAssembler.h
+++ gsThinShellAssembler.h
@@ -320,7 +320,7 @@ private:
 
     /// Implementation of the boundary force vector for surfaces (3D)
     template<short_t _d, bool _bending>
-    typename std::enable_if<_d==3 && _bending, gsMatrix<T> >::type
+    typename std::enable_if<(_d==3) && _bending, gsMatrix<T> >::type
     boundaryForce_impl(const gsFunctionSet<T>   & deformed , patchSide& ps);
 
     /// Implementation of the boundary force vector for planar geometries (2D)
