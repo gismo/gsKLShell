1a75f5c81172b6737b6715d0fc4d55466055fe8a
Matthias Moller <mmoelle1@gmail.com>
2024-03-04 18:25:23 +0200

    Fix bugs in includes

--- gsKLShell.h
+++ gsKLShell.h
@@ -1,14 +1,23 @@
 #include <gsCore/gsTemplateTools.h>
 
+#include <gsKLShell/src/getMaterialMatrix.h>
+
 #include <gsKLShell/src/gsMaterialMatrixBase.h>
 #include <gsKLShell/src/gsMaterialMatrixBaseDim.h>
-
+#include <gsKLShell/src/gsMaterialMatrixComposite.h>
+#include <gsKLShell/src/gsMaterialMatrixEval.h>
+#include <gsKLShell/src/gsMaterialMatrixIntegrate.h>
 #include <gsKLShell/src/gsMaterialMatrixLinear.h>
 #include <gsKLShell/src/gsMaterialMatrixNonlinear.h>
+#include <gsKLShell/src/gsMaterialMatrixTFT.h>
+#include <gsKLShell/src/gsMaterialMatrixUtils.h>
+#include <gsKLShell/src/gsMaterialMatrixXml.h>
 
 #include <gsKLShell/src/gsThinShellAssembler.h>
 #include <gsKLShell/src/gsThinShellAssemblerDWR.h>
 
+#include <gsKLShell/src/gsThinShellFunctions.h>
+#include <gsKLShell/src/gsThinShellUtils.h>
 
 namespace gismo
 {
