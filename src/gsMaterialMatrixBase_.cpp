b5da9f121ffecc13558fb1bed7b9bd134a0c825e
Angelos Mantzaflaris (Άγγελος Μαντζαφλάρης) <filiatra@gmail.com>
2025-01-04 05:41:14 +0100

    remove old bindings

--- src/gsMaterialMatrixBase_.cpp
+++ src/gsMaterialMatrixBase_.cpp
@@ -39,22 +39,6 @@ namespace gismo
     ;
   }
 
-  void pybind11_init_gsMaterialMatrixBaseDim2(py::module &m)
-  {
-    using Base  = gsMaterialMatrixBase<real_t>;
-    using Class = gsMaterialMatrixBaseDim<2,real_t>;
-    py::class_<Class,Base>(m, "gsMaterialMatrixBaseDim2")
-    ;
-  }
-
-  void pybind11_init_gsMaterialMatrixBaseDim3(py::module &m)
-  {
-    using Base  = gsMaterialMatrixBase<real_t>;
-    using Class = gsMaterialMatrixBaseDim<3,real_t>;
-    py::class_<Class,Base>(m, "gsMaterialMatrixBaseDim3")
-    ;
-  }
-
   #endif
 
 }
