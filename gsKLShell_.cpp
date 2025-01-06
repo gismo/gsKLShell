6b222ac95c70105b503d1d3f452e7ffd75c9591a
Hugo Verhelst <hugoverhelst1995@gmail.com>
2025-01-06 10:43:50 +0100

    small pybind conflict fixes in unittests

---
+++ gsKLShell_.cpp
@@ -0,0 +1,59 @@
+#include <gsCore/gsTemplateTools.h>
+
+
+namespace gismo
+{
+
+#ifdef GISMO_WITH_PYBIND11
+
+  namespace py = pybind11;
+
+  void pybind11_init_gsKLShell(py::module &m)
+  {
+    gismo::pybind11_init_gsMaterialMatrixBase( m );
+
+    gismo::pybind11_init_gsMaterialMatrixBaseDim2( m );
+    gismo::pybind11_init_gsMaterialMatrixBaseDim3( m );
+
+    gismo::pybind11_init_gsMaterialMatrixLinear2( m );
+    gismo::pybind11_init_gsMaterialMatrixLinear3( m );
+
+    gismo::pybind11_init_gsMaterialMatrixNH2i( m );
+    gismo::pybind11_init_gsMaterialMatrixNH2c( m );
+
+    gismo::pybind11_init_gsMaterialMatrixNH3i( m );
+    gismo::pybind11_init_gsMaterialMatrixNH3c( m );
+
+    gismo::pybind11_init_gsMaterialMatrixMR2i( m );
+    gismo::pybind11_init_gsMaterialMatrixMR2c( m );
+
+    gismo::pybind11_init_gsMaterialMatrixMR3i( m );
+    gismo::pybind11_init_gsMaterialMatrixMR3c( m );
+
+    gismo::pybind11_init_gsMaterialMatrixOG2i( m );
+    gismo::pybind11_init_gsMaterialMatrixOG2c( m );
+
+    gismo::pybind11_init_gsMaterialMatrixOG3i( m );
+    gismo::pybind11_init_gsMaterialMatrixOG3c( m );
+
+    gismo::pybind11_init_gsThinShellAssemblerBase( m );
+    gismo::pybind11_enum_gsThinShellAssemblerStatus( m );
+
+    gismo::pybind11_init_gsThinShellAssembler2( m );
+    gismo::pybind11_init_gsThinShellAssembler3( m );
+    gismo::pybind11_init_gsThinShellAssembler3nb( m );
+
+    gismo::pybind11_enum_GoalFunction( m );
+
+    gismo::pybind11_init_gsThinShellAssemblerDWRBase( m );
+
+    gismo::pybind11_init_gsThinShellAssemblerDWR2( m );
+    gismo::pybind11_init_gsThinShellAssemblerDWR3( m );
+    gismo::pybind11_init_gsThinShellAssemblerDWR3nb( m );
+  }
+
+#endif
+
+
+}
+
--- src/gsMaterialMatrixBaseDim_.cpp
+++ src/gsMaterialMatrixBaseDim_.cpp
@@ -8,5 +8,27 @@ namespace gismo
   CLASS_TEMPLATE_INST gsMaterialMatrixBaseDim<2,real_t>;
   CLASS_TEMPLATE_INST gsMaterialMatrixBaseDim<3,real_t>;
 
+  #ifdef GISMO_WITH_PYBIND11
+
+  namespace py = pybind11;
+
+  void pybind11_init_gsMaterialMatrixBaseDim2(py::module &m)
+  {
+    using Base  = gsMaterialMatrixBase<real_t>;
+    using Class = gsMaterialMatrixBaseDim<2,real_t>;
+    py::class_<Class,Base>(m, "gsMaterialMatrixBaseDim2")
+    ;
+  }
+
+  void pybind11_init_gsMaterialMatrixBaseDim3(py::module &m)
+  {
+    using Base  = gsMaterialMatrixBase<real_t>;
+    using Class = gsMaterialMatrixBaseDim<3,real_t>;
+    py::class_<Class,Base>(m, "gsMaterialMatrixBaseDim3")
+    ;
+  }
+
+  #endif
+
 }
 
